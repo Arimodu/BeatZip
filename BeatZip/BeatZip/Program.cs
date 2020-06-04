e023c5517a5463028103bcf4e2f506c83a332c6d
Arimodu <44728973+Arimodu@users.noreply.github.com>
2020-06-04 09:53:06 +0200

    Add files via upload

---
+++ BeatZip/BeatZip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace BeatZip
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string PCVRInstallDir;
+            string destFolder;
+            Console.Write("Enter your PCVR Beat Saber installation directory: ");
+            PCVRInstallDir = Console.ReadLine();
+            Console.Write("Enter your destination folder (the folder where to put all the zips): ");
+            destFolder = Console.ReadLine();
+
+            string PCVRSongDir = PCVRInstallDir + @"\Beat Saber_Data\CustomLevels";
+            string[] subdirs = Directory.GetDirectories(PCVRSongDir);
+
+            for (int i = 0; i < subdirs.Length; i++)
+            {
+                Console.WriteLine(subdirs[i]);
+                Console.WriteLine("Zipping!");
+                ZipFile.CreateFromDirectory(subdirs[i], destFolder + @"\" + subdirs[i].Substring(73) + ".zip");
+                Console.WriteLine("Zipped succesfully!");
+            }
+            Console.WriteLine("All files zipped succesfully");
+            Console.ReadKey();
+        }
+    }
+}
